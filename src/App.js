ff5e77cf614912fb070d1a53cffb8653dd367038
nunupy345345 <airinota@outlook.jp>
2023-10-12 13:23:34 +0900

    git push add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+//1．useStateとuseEffectをインポート
+import { useState,useEffect } from 'react';
 
 function App() {
+  //useStateの初期値（空）を設定
+  const [message, setMessage] = useState('');
+  
+  useEffect(() => {
+    //fetchでバックエンドExpressのサーバーを指定
+    fetch('/api')
+      //レスポンスをjsonとして受け取りjsオブジェクトを生成
+      .then((res) => res.json())
+      //生成したjsオブジェクトをdataに代入
+      //data.messageで取り出したデータをuseStateに保存
+      .then((data) => setMessage(data.message));
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>フロントエンド</h1>
+      //useStateに保存した値を表示
+      <p>{ message }</p>
     </div>
   );
 }
